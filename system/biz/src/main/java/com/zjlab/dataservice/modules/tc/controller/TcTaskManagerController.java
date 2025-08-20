497f1edb192b976c08b6ee5eab77103a1270fe19
ded <121849853+liujingjie3@users.noreply.github.com>
2025-08-20 08:58:47 +0800

    feat: add swagger docs for task manager

--- system/biz/src/main/java/com/zjlab/dataservice/modules/tc/controller/TcTaskManagerController.java
+++ system/biz/src/main/java/com/zjlab/dataservice/modules/tc/controller/TcTaskManagerController.java
@@ -8,17 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import javax.annotation.Resource;
 
 /** 任务列表接口 */
 @RestController
 @RequestMapping("/task")
+@Api(tags="任务列表")
 public class TcTaskManagerController {
 
     @Resource
     private TaskManagerService taskManagerService;
 
     @GetMapping("/list")
+    @ApiOperation(value="任务列表", notes="分页查询任务列表")
     public Result<TaskManagerListPageVO> list(TaskManagerListQuery query) {
         TaskManagerListPageVO page = taskManagerService.listTasks(query);
         return Result.ok(page);
