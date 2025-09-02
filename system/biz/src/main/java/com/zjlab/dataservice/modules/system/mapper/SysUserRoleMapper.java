44bdb4f748ec014c1a8adae64cfbae412ef46573
ded <121849853+liujingjie3@users.noreply.github.com>
2025-09-02 16:37:00 +0800

    by pw

--- system/biz/src/main/java/com/zjlab/dataservice/modules/system/mapper/SysUserRoleMapper.java
+++ system/biz/src/main/java/com/zjlab/dataservice/modules/system/mapper/SysUserRoleMapper.java
@@ -38,6 +38,6 @@ public interface SysUserRoleMapper extends BaseMapper<SysUserRole> {
 	 * @return List<String>
 	 */
 	@Select("select role_code from sys_role where id in (select role_id from sys_user_role where user_id = #{userid})")
-	List<String> getRoleByUserId(@Param("username") String userid);
+	List<String> getRoleByUserId(@Param("userid") String userid);
 
 }
