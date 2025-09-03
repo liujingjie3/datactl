1a13b050582f70c8219435256b9b17dcc7525105
ded <121849853+liujingjie3@users.noreply.github.com>
2025-09-03 14:46:35 +0800

    + 序列化

--- system/biz/src/main/java/com/zjlab/dataservice/modules/tc/model/dto/TaskStatusCountDto.java
+++ system/biz/src/main/java/com/zjlab/dataservice/modules/tc/model/dto/TaskStatusCountDto.java
@@ -2,11 +2,14 @@ package com.zjlab.dataservice.modules.tc.model.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 状态任务数量
  */
 @Data
-public class TaskStatusCountDto {
+public class TaskStatusCountDto  implements Serializable {
+    private static final long serialVersionUID = -2857439168225563040L;
     /** 任务状态 */
     private Integer status;
     /** 数量 */
--- system/biz/src/main/java/com/zjlab/dataservice/modules/tc/model/vo/TaskCountVO.java
+++ system/biz/src/main/java/com/zjlab/dataservice/modules/tc/model/vo/TaskCountVO.java
@@ -2,11 +2,14 @@ package com.zjlab.dataservice.modules.tc.model.vo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 任务数量统计
  */
 @Data
-public class TaskCountVO {
+public class TaskCountVO  implements Serializable {
+    private static final long serialVersionUID = -5978063147559715091L;
     /** 总任务数 */
     private Long total;
     /** 运行中任务数 */
