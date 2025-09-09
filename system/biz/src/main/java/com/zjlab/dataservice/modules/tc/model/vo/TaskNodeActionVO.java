80ca78b2b6e858e076e54073f91fe236b9250932
ded <121849853+liujingjie3@users.noreply.github.com>
2025-09-09 16:53:55 +0800

    feat: include remote data in task detail

--- system/biz/src/main/java/com/zjlab/dataservice/modules/tc/model/vo/TaskNodeActionVO.java
+++ system/biz/src/main/java/com/zjlab/dataservice/modules/tc/model/vo/TaskNodeActionVO.java
@@ -3,7 +3,6 @@ package com.zjlab.dataservice.modules.tc.model.vo;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 
 /** 节点操作记录信息 */
 @Data
@@ -12,12 +11,6 @@ public class TaskNodeActionVO implements Serializable {
     private static final long serialVersionUID = 8431966549122817451L;
     /** 节点实例ID */
     private Long nodeInstId;
-    /** 操作时间 */
-    private LocalDateTime actionTime;
-    /** 操作人ID */
-    private String operatorId;
-    /** 操作人名称 */
-    private String operatorName;
     /** 操作类型 */
     private Integer actionType;
     /** 提交内容JSON */
