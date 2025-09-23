c3dd32d89e654cbb5ea5cf0b0a6c1a1398394111
ded <121849853+liujingjie3@users.noreply.github.com>
2025-09-23 15:48:43 +0800

    +rollback

--- system/biz/src/main/java/com/zjlab/dataservice/modules/tc/model/dto/TcBaseEntityDto.java
+++ system/biz/src/main/java/com/zjlab/dataservice/modules/tc/model/dto/TcBaseEntityDto.java
@@ -1,8 +1,6 @@
 package com.zjlab.dataservice.modules.tc.model.dto;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
+
 import lombok.Data;
 
 import java.io.Serializable;
@@ -15,51 +13,17 @@ import java.time.LocalDateTime;
 public class TcBaseEntityDto implements Serializable {
 
     private static final long serialVersionUID = 5014487643533278311L;
-
-    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
-
-    /** 删除标识 */
-    @TableLogic
-    private Boolean delFlag;
-
     /** 创建人ID */
-    private String createBy;
+    private String creatorId;
     /** 创建人名称 */
     private String creatorName;
     /** 创建时间 */
     private LocalDateTime createTime;
     /** 更新人ID */
-    private String updateBy;
+    private String updaterId;
     /** 更新人名称 */
     private String updaterName;
     /** 更新时间 */
     private LocalDateTime updateTime;
-
-    /**
-     * 初始化基础信息
-     *
-     * @param save 是否保存操作
-     * @param userId 操作用户
-     */
-    public void initBase(boolean save, String userId) {
-        if (save) {
-            this.setDelFlag(Boolean.FALSE);
-            this.setCreateBy(userId);
-            this.setCreateTime(LocalDateTime.now());
-        }
-        this.setUpdateBy(userId);
-        this.setUpdateTime(LocalDateTime.now());
-    }
-
-    /**
-     * 标记删除
-     *
-     * @param userId 操作用户
-     */
-    public void del(String userId) {
-        this.setDelFlag(Boolean.TRUE);
-        this.setUpdateBy(userId);
-        this.setUpdateTime(LocalDateTime.now());
-    }
 }
